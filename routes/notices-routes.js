3e71cc43cbb1baebc0c4fa6279cefbdb7db0a51c
Anton Krupin <krupin_anton@mail.ru>
2023-10-08 18:02:55 +0300

    add routes to edit and get notice by id

--- routes/notices-routes.js
+++ routes/notices-routes.js
@@ -6,7 +6,11 @@ const router = express.Router();
 
 router.get('/', noticeControllers.getNotices);
 
+router.get('/editNotice/:id', noticeControllers.getNoticeById);
+
 router.post('/addNotice', noticeControllers.addNotice);
+
+router.patch('/editNotice/:id', noticeControllers.updateNoticeById);
 
 router.delete('/notice/:id', noticeControllers.deleteNoticeById);
 
