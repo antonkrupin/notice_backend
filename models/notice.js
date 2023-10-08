9ff7a7531176126190a1b1393f675f254a2cd477
Anton Krupin <krupin_anton@mail.ru>
2023-10-08 18:02:00 +0300

    add editDate field

--- models/notice.js
+++ models/notice.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const noticeSchema = new Schema({
   body: {type: String, required: true},
   creationDate: {type: String, required: true},
+  editDate: {type: String, required: false},
 });
 
 module.exports = mongoose.model('Notice', noticeSchema);
